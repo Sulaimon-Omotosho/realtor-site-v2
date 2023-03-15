f94463b87573be4bc6c4220f26c41a0a007cc825
Susudef <Susudef2u@gmail.com>
2023-03-15 02:30:36 +0100

    Listings Edit and Delete functionality

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Moment from 'react-moment'
 import { Link } from 'react-router-dom'
-import { MdLocationOn } from 'react-icons/md'
+import { MdLocationOn, MdEdit } from 'react-icons/md'
+import { FaTrash } from 'react-icons/fa'
 
-export default function ListingItem({ listing, id }) {
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
   return (
     <li class='relative bg-white flex flex-col justify-between items-center shadow-md over:shadow-xl rounded-md overflow-hidden transition duration-105 m-[10px]'>
       <Link class='contents' to={`/category/${listing.type}/${id}`}>
@@ -54,6 +55,18 @@ export default function ListingItem({ listing, id }) {
           </div>
         </div>
       </Link>
+      {onDelete && (
+        <FaTrash
+          class='absolute bottom-2 right-2 h-[14px] cursor-pointer text-red-500'
+          onClick={() => onDelete(listing.id)}
+        />
+      )}
+      {onEdit && (
+        <MdEdit
+          class='absolute bottom-2 right-7 h-4 cursor-pointer'
+          onClick={() => onEdit(listing.id)}
+        />
+      )}
     </li>
   )
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { getAuth, updateProfile } from 'firebase/auth'
 import {
   collection,
+  deleteDoc,
   doc,
   getDocs,
   orderBy,
@@ -80,6 +81,19 @@ export default function Profile() {
     }
     fetchUserListings()
   }, [auth.currentUser.uid])
+  async function onDelete(listingID) {
+    if (window.confirm('Click OK to delete')) {
+      await deleteDoc(doc(db, 'listings', listingID))
+      const updatedListings = listings.filter(
+        (listing) => listing.id !== listingID
+      )
+      setListings(updatedListings)
+      toast.success('Deleted Successfully')
+    }
+  }
+  function onEdit(listingID) {
+    navigate(`/edit-listing/${listingID}`)
+  }
 
   return (
     <>
@@ -152,6 +166,8 @@ export default function Profile() {
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
+                  onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
