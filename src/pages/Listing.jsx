599ef4daf708739a891f1c3e0d6ad3564d20fa40
Susudef <Susudef2u@gmail.com>
2023-03-16 14:27:39 +0100

    Owner contact Component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -19,12 +19,16 @@ import {
   FaParking,
   FaChair,
 } from 'react-icons/fa'
+import { getAuth } from 'firebase/auth'
+import Contact from '../components/Contact'
 
 export default function Listing() {
+  const auth = getAuth()
   const params = useParams()
   const [listing, setListing] = useState(null)
   const [loading, setLoading] = useState(true)
   const [shareLinkCopied, setShareLinkCopied] = useState(false)
+  const [contactOwner, setContactOwner] = useState(false)
   SwiperCore.use([Autoplay, Navigation, Pagination])
 
   useEffect(() => {
@@ -83,7 +87,7 @@ export default function Listing() {
       )}
 
       <div className='m-4 flex flex-col md:flex-row max-w-6xl lg:mx-auto p-4 rounded-lg shadow-lg bg-white lg:space-x-5'>
-        <div className=' w-full h-[200px] lg-[400px]'>
+        <div className=' w-full '>
           <p class='text-2xl font-bold mb-3 text-blue-900'>
             {listing.name} - N{' '}
             {listing.offer
@@ -115,7 +119,7 @@ export default function Listing() {
             <p class='font-semibold'>Description</p>
             <p class=''>{listing.description}</p>
           </div>
-          <ul class='flex items-center space-x-2 sm:space-x-10 text-sm font-semibold'>
+          <ul class='flex items-center space-x-2 sm:space-x-10 text-sm font-semibold mb-6'>
             <li class='flex items-center whitespace-nowrap'>
               <FaBed class='text-lg mr-1' />
               {listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : '1 Bed'}
@@ -133,6 +137,19 @@ export default function Listing() {
               {listing.furnished ? 'Furnished' : 'Not Furnished'}
             </li>
           </ul>
+          {listing.userRef !== auth.currentUser?.uid && !contactOwner && (
+            <div className='mt-6 '>
+              <button
+                onClick={() => setContactOwner(true)}
+                class='px-7 py-3 bg-blue-600 text-white font-medium text-sm uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg w-full text-center transition duration-150 ease-in-out'
+              >
+                Contact Owner
+              </button>
+            </div>
+          )}
+          {contactOwner && (
+            <Contact userRef={listing.userRef} listing={listing} />
+          )}
         </div>
         <div className='bg-blue-300 w-full h-[200px] lg-[400px] z-10 '></div>
       </div>
